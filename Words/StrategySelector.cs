0c8370ef40eca0085ac170f99b97aa6fb37ef572
astovpyak <stovpyak@gmail.com>
2016-06-22 00:19:13 +0800

    update ignore

---
+++ Words/StrategySelector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Model.Strategy;
+using Model.StrategyContracts;
+
+namespace Words
+{
+    public class StrategySelector: IStrategySelector
+    {
+        public IPlayStrategy Select(List<IPlayStrategy> strategies)
+        {
+            Console.WriteLine("Выберите режим:");
+
+            for (int i = 0; i < strategies.Count; i++)
+            {
+                Console.WriteLine($"{i+1}) {strategies[i].Name}");
+            }
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                int index = int.Parse(input) - 1;
+                if ((index >= 0) && (index < strategies.Count))
+                {
+                    return strategies[index];
+                }
+                else
+                    Console.WriteLine($"Вы ввели некорректное число. Попробуйте ещё раз...");
+            }
+        }
+    }
+}
